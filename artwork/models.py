e52a1568837e1144889ce7f5586f4a85f5222519
Hye-Ji Na <nhj7911@gmail.com>
2021-11-26 03:32:56 +0900

    Work 모델 생성 및 시간설정

--- artwork/models.py
+++ artwork/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Work(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    price = models.CharField(max_length=100)
+    # author
+    # category
+    # tags
+    commericial = models.CharField(max_length=100)
+
+    #thumnail = models.ImageField(upload_to='artworks/images/%Y/%M/%d/', blank=True)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self): # 상세 포스트주소
+        return f'/artwork/{self.pk}'
--- shoppingmall/urls.py
+++ shoppingmall/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    path('artwork/', include('artwork.urls')),
     path('admin/', admin.site.urls),
+    path('single_pages/',include('single_pages.urls'))
 ]
+
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # 서버IP/media/
