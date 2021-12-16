eb1f63e065ae224d4f67b47e10b1922c87f6235e
Hye-Ji Na <nhj7911@gmail.com>
2021-12-16 23:46:44 +0900

    홈페이지에 최근 포스트 3개 보여주기

--- single_pages/views.py
+++ single_pages/views.py
@@ -3,9 +3,9 @@ from artwork.models import Work
 
 # Create your views here.
 def langing(request):
-    #recent_posts = Work.objects.order_by('-pk')[:3] # 최신 포스트
-    return render(request, 'single_pages/home.html')
-                #  {'recent_posts' : recent_posts})
+    recent_works = Work.objects.order_by('-pk')[:3] # 최신 포스트
+    return render(request, 'single_pages/home.html',
+                 {'recent_works' : recent_works})
 
 def about_us(request):
     return render(request, 'single_pages/about_us.html')
