c6d1cb1dd953dfeea0130312616900e498745162
Hye-Ji Na <nhj7911@gmail.com>
2021-12-18 09:54:04 +0900

    도커에 장고만 담기

--- shoppingmall/settings.py
+++ shoppingmall/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ii-b4qbn6g^o%_(x3+h=1cxng#4h8@&t6l(kxh80)+y#y=t+nq'
+SECRET_KEY = os.environ.get('SECRECT_KEY','django-insecure-ii-b4qbn6g^o%_(x3+h=1cxng#4h8@&t6l(kxh80)+y#y=t+nq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG',1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else :
+    ALLOWED_HOSTS = []
 
 
 # Application definition
